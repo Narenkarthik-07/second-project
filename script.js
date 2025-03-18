423742860db2525ceff9ba9c9f2e6df9d88d1c38
Naren Karthik <narenkarthik422@gmail.com>
2025-03-18 08:44:13 -0700

    project initiated

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-toggle').addEventListener('click',(f)=>{
+    document.querySelector('.nav-links').classList.toggle('active');
+})
+document.getElementById('contactForm').addEventListener('submit', (e)=>{
+    e.preventDefault():
+    alert("Thank You")
+})
